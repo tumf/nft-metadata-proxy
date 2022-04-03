6f2acebb1ea8211b62e8b959976aeeefc680c956
tumf <y.takahara@gmail.com>
2022-04-03 09:00:04 +0700

    initial version

---
+++ index.js
@@ -0,0 +1,67 @@
+const RE_ORIGINS = ".*";
+const CACHE_TTL = 600;
+
+addEventListener("fetch", event => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      err => new Response(err.stack, { status: 500 }),
+    ),
+  );
+});
+
+function MethodNotAllowed(request) {
+  return new Response(`Method ${request.method} not allowed.`, {
+    status: 405,
+    headers: {
+      Allow: "GET",
+    },
+  });
+}
+
+async function handleRequest(request) {
+  const origin = request.headers.get("Origin");
+  if (origin != null && !origin.match(RE_ORIGINS)) {
+    origin = "*";
+  }
+
+  if (request.method == "OPTIONS") {
+    return new Response(null, { headers: { Allow: "GET, OPTIONS" } });
+  }
+
+  if (request.method !== "GET") return MethodNotAllowed(request);
+
+  const params = new URL(request.url).searchParams;
+  const url = params.get("url");
+  console.log(url);
+  if (!url) {
+    return new Response(null, {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+  try {
+    const resp = await fetch(url, {
+      cf: {
+        cacheTtl: CACHE_TTL,
+        cacheEverything: true,
+        cacheKey: url,
+      },
+    });
+    const json = await resp.text();
+
+    return new Response(json, {
+      headers: {
+        Vary: "Origin",
+        "Cache-Control": `max-age=${CACHE_TTL}`,
+        "Access-Control-Allow-Origin": origin,
+        "Access-Control-Allow-Methods": "GET",
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {
+    return new Response(null, {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+}
